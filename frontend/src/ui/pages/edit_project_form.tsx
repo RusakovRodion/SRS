4a5386e2350f9732b699a68de6ce4fca31a5c55e
genia10 <61140036+genia10@users.noreply.github.com>
2024-11-21 11:12:56 +0300

    submit form

--- frontend/src/ui/pages/edit_project_form.tsx
+++ frontend/src/ui/pages/edit_project_form.tsx
@@ -1,19 +1,55 @@
 import "./page.css";
 import { useParams } from 'react-router-dom';
 import {projects } from "./../fake_backend_data"
+import { Project } from "./../project"
+import { SaveButton } from "./../buttons"
+import { SubmitHandler, useForm } from "react-hook-form";
+
+type FormValues = {
+  name: string,
+  type: string,
+  description: string
+}
+
+function findNextId(objects:any) {
+    const ids = objects.map(object => {
+            return object.id;
+        });
+    if (objects.length == 0)
+        return 1
+    else
+        return Math.max(...ids) + 1;
+}
 
 export function EditProjectForm(mode: string) {
+
+    const { register, handleSubmit } = useForm<FormValues>()
+    const onSubmit: SubmitHandler<FormValues> = (data) => {
+        console.log(data)
+        console.log(findNextId(projects));
+        projects.push({
+            'name': data['name'],
+            'type': data['type'],
+            'description': data['description'],
+        })
+        console.log(projects)
+    }
+
+
     if (mode['mode'] === 'new'){
         return (
             <div className="content">
                 <h2>Новый проект</h2>
                 <form id="edit_project_form">
                     <h3>Название</h3>
-                    <input type="text" placeholder="Название"/>
+                    <input type="text" id="name" {...register("name")} placeholder="Название" required/>
                     <h3>Тип</h3>
-                    <input type="text" placeholder="Тип"/>
+                    <input type="text" id="type" {...register("type")} placeholder="Тип" required/>
                     <h3>Описание</h3>
-                    <input type="text" placeholder="Описание"/>
+                    <input type="text" id="description" {...register("description")} placeholder="Описание" required/>
+                    <div className="save_btn_block">
+                        <SaveButton onClick={handleSubmit(onSubmit)}/>
+                    </div>
                 </form>
             </div>
         );

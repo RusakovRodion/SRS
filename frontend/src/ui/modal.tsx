0759060c8f912761e0638eeb2a623a30dd45244a
Nikon Podgorny <podgorny.nikon@yandex.ru>
2024-11-30 20:00:38 +0300

    fixes for modal

--- frontend/src/ui/modal.tsx
+++ frontend/src/ui/modal.tsx
@@ -1,61 +1,93 @@
 import React, { useRef, useEffect, useState } from "react";
-import "./modal.module.css";
+import { modal, modal_close_btn } from "./modal.module.css";
 
 interface ModalProps {
-  isOpen: boolean;
-  hasCloseBtn?: boolean;
-  onClose?: () => void;
-  children: React.ReactNode;
-};
+    isOpen: boolean;
+    hasCloseBtn?: boolean;
+    onClose?: () => void;
+    children: React.ReactNode;
+}
 
 const Modal: React.FC<ModalProps> = ({
-  isOpen,
-  hasCloseBtn = true,
-  onClose,
-  children
+    isOpen,
+    hasCloseBtn = true,
+    onClose,
+    children,
 }) => {
-  const [isModalOpen, setModalOpen] = useState(isOpen);
-  const modalRef = useRef<HTMLDialogElement | null>(null);
+    const [isModalOpen, setModalOpen] = useState(isOpen);
+    const modalRef = useRef<HTMLDialogElement | null>(null);
 
-  const handleCloseModal = () => {
-    if (onClose) {
-      onClose();
-    }
-    setModalOpen(false);
-  };
+    const handleCloseModal = () => {
+        if (onClose) {
+            onClose();
+        }
+        setModalOpen(false);
+    };
 
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLDialogElement>) => {
-    if (event.key === "Escape") {
-      handleCloseModal();
-    }
-  };
+    const handleKeyDown = (event: React.KeyboardEvent<HTMLDialogElement>) => {
+        if (event.key === "Escape") {
+            handleCloseModal();
+        }
+    };
 
-  useEffect(() => {
-    setModalOpen(isOpen);
-  }, [isOpen]);
+    useEffect(() => {
+        setModalOpen(isOpen);
+    }, [isOpen]);
 
-  useEffect(() => {
-    const modalElement = modalRef.current;
+    useEffect(() => {
+        const modalElement = modalRef.current;
 
-    if (modalElement) {
-      if (isModalOpen) {
-        modalElement.showModal();
-      } else {
-        modalElement.close();
-      }
-    }
-  }, [isModalOpen]);
+        if (modalElement) {
+            if (isModalOpen) {
+                modalElement.showModal();
+            } else {
+                modalElement.close();
+            }
+        }
+    }, [isModalOpen]);
 
-  return (
-    <dialog ref={modalRef} onKeyDown={handleKeyDown} className="modal">
-      {hasCloseBtn && (
-        <button className="modal-close-btn" onClick={handleCloseModal}>
-          Close
-        </button>
-      )}
-      {children}
-    </dialog>
-  );
+    const handleClickOutside = (e: MouseEvent) => {
+        if (modalRef.current) {
+            // NOTE: т.к. мы используем нативный <dialog> обработка выглядит как-то так...
+            // для реакта более адекватного решения не нашёл
+            const rect = modalRef.current.getBoundingClientRect();
+            var isInDialog =
+                rect.top <= e.clientY &&
+                e.clientY <= rect.top + rect.height &&
+                rect.left <= e.clientX &&
+                e.clientX <= rect.left + rect.width;
+
+            if (!isInDialog) {
+                if (onClose) {
+                    onClose();
+                }
+            }
+        }
+    };
+
+    // Обработка клика ВНЕ формы
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    return (
+        <div style={{}}>
+            <dialog ref={modalRef} onKeyDown={handleKeyDown} className={modal}>
+                {hasCloseBtn && (
+                    <button
+                        className={modal_close_btn}
+                        onClick={handleCloseModal}
+                    >
+                        Close
+                    </button>
+                )}
+                {children}
+            </dialog>
+        </div>
+    );
 };
 
 export default Modal;
